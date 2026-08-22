e23cc4e36725123673c58d38a49d664d47f43aac
ibrahimshoshaaa <brahimsosa9@gmail.com>
2026-08-22 21:44:13 +0300

    Update welcome_screen.dart

--- lib/features/onboarding/presentation/screens/welcome_screen.dart
+++ lib/features/onboarding/presentation/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../../../core/theme/app_theme.dart';
+import '../../../../core/constants/app_colors.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
